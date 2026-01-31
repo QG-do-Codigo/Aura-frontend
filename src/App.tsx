87cd3c5dd77351144be3f88579f211f7483e358b
isabeldiana <isabel_diana@outlook.com>
2026-01-30 22:11:46 -0300

    feat(ui): add Radix UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from './components/UI/button'
+
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-bold text-primary">Hello World</h1>
+      <div className="flex flex-row justify-center items-center gap-10 mt-10"></div>
+      <Button variant={'default'} size={'lg'}>
+        +
+      </Button>
     </>
   )
 }

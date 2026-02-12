133eb50d2c738aa9d48b2713515bc83f553b00d9
Geana-Almeida <geana.almeida2000@gmail.com>
2026-02-12 20:02:52 -0300

    feat: implement authentication flow with login, private routes, and toast notifications

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -11,24 +11,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "../pages/auth/signIn";
 import SignUp from "../pages/auth/signUp";
 import { NotesPage } from "../pages/notes/components/notesPage";
+import { PrivateRoute } from "./PrivateRoute";
+import { ToastContainer } from "react-toastify";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        {'Rotas publicas'}
         <Route path="/login" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
-        <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="tasks" element={<TasksPage />} />
-          <Route path="notes" element={<NotesPage />} />
-          <Route path="shopping" element={<ShoppingPage />} />
-          <Route path="health" element={<HealthPage />} />
-          <Route path="finance" element={<FinancePage />} />
-          <Route path="sleep" element={<SleepPage />} />
-          <Route path="ideas" element={<IdeasPage />} />
+
+        {'Rotas privadas'}
+        <Route path="/dashboard" element={<PrivateRoute><DashboardLayout/></PrivateRoute>}>
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="tasks" element={<PrivateRoute><TasksPage /></PrivateRoute>} />
+        <Route path="notes" element={<PrivateRoute><NotesPage /></PrivateRoute>} />
+        <Route path="shopping" element={<PrivateRoute><ShoppingPage /></PrivateRoute>} />
+        <Route path="health" element={<PrivateRoute><HealthPage /></PrivateRoute>} />
+        <Route path="finance" element={<PrivateRoute><FinancePage /></PrivateRoute>} />
+        <Route path="sleep" element={<PrivateRoute><SleepPage /></PrivateRoute>} />
+        <Route path="ideas" element={<PrivateRoute><IdeasPage /></PrivateRoute>} />
         </Route>
       </Routes>
+      <ToastContainer />
     </BrowserRouter>
   );
 }
---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import api from "../api";
+
+export interface SignDto {
+  email: string,
+  password: string
+}
+
+export interface SignInResponse {
+  email:string,
+  password: string,
+  token: string
+}
+
+export const authService = {
+
+  async signIn(data: SignDto): Promise<SignInResponse> {
+    const response = await api.post<SignInResponse>("/auth/signin", data);
+
+    localStorage.setItem("token", response.data.token)
+
+    return response.data
+  
+  },
+
+
+  async LogOut(){
+    localStorage.removeItem("token")
+  },
+
+  getToken() {
+    return localStorage.getItem("token");
+  }
+}
